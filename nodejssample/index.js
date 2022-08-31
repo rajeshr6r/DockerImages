990f194dc8f60484f23238230767cdee2c6b7fff
rajeshr6r <rajesh.r6r@gmail.com>
2022-08-31 12:44:49 +0200

    changes for docker-compose prod and dev

--- nodejssample/index.js
+++ nodejssample/index.js
@@ -1,7 +1,9 @@
+
+express = require('express');
 const app = express();
 const port = process.env.PORT || 3500
-app.get('/', (req,res)  => {
-    res.sendStatus(200).send("<h2>Hello there<h2")
+app.get('/', (req, res) => {
+    res.status(200).send("<h2>Hello There world <h2")
 });
 
-app.listen(port,() => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
